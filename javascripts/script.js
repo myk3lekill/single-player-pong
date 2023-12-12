4cfe6113acd51308cbe995a10effb85301fca3df
me <me@gmail.com>
2023-12-12 18:35:29 +0100

    seventh commit: Handling events in the pong client

--- javascripts/script.js
+++ javascripts/script.js
@@ -2,6 +2,7 @@
 const canvas = document.createElement('canvas');
 const context = canvas.getContext('2d');
 const socket = io('http://localhost:3000');
+let isReferee = false;
 let paddleIndex = 0;
 
 let width = 500;
@@ -183,13 +184,14 @@ function animate() {
   window.requestAnimationFrame(animate);
 }
 
-// Start Game, Reset Everything
-function startGame() {
+// Load Game, Reset Everything
+function loadGame() {
   createCanvas();
   renderIntro();
   socket.emit('ready', {})
-  
-  paddleIndex = 0;
+}
+function startGame() { 
+  paddleIndex = isReferee ? 0 : 1;
   window.requestAnimationFrame(animate);
   canvas.addEventListener('mousemove', (e) => {
     playerMoved = true;
@@ -206,8 +208,19 @@ function startGame() {
 }
 
 // On Load
-startGame();
+loadGame();
 
-socket.on('connect', (socket) => {
+//--------- Event Handlers ---------//
+//On Connect
+socket.on('connect', () => {
   console.log('Connected as...', socket.id)
+})
+
+//On startGame
+socket.on('startGame', (refereeId) => {
+  console.log('Referee is', refereeId);
+
+  isReferee = socket.id === refereeId
+  //Start Game
+  startGame()
 })
