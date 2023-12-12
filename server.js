32d952b5f1a0659b3ce592e4f6c70e37baa64a7c
me <me@gmail.com>
2023-12-12 17:21:34 +0100

    fifth commit: Listening for Events in Pong Server

--- server.js
+++ server.js
@@ -11,7 +11,21 @@ const PORT = 3000;
 server.listen(PORT);
 console.log(`Listening on port ${PORT}...`);
 
+//Track Players
+let readyPlayerCount = 0;
+
 //Event Emitter
 io.on('connection', (socket) => {
-    console.log('A user connected', socket.id)
+    console.log('A user connected', socket.id);
+
+    socket.on('ready', () => {
+        console.log('Player ready', socket.id)
+
+        //Track Players
+        readyPlayerCount++;
+
+        if (readyPlayerCount === 2) {
+            //broadcast ('startGame')
+        }
+    })
 })
